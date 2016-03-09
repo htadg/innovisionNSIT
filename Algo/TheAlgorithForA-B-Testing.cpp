06d1db87e4cf05898253fb7918192690c158c7cc
Vignesh M <vigzmv@outlook.com>
2016-03-10 03:25:37 +0530

    ALGO

--- Algo/TheAlgorithForA-B-Testing.cpp
+++ Algo/TheAlgorithForA-B-Testing.cpp
@@ -1,6 +1,8 @@
 #include "iostream"
 #include "stdio.h"
 #include "stdlib.h"
+#include "time.h"
+
 using namespace std;
 
 int data[10000][3] = {0};
@@ -8,6 +10,7 @@ int pos=0;
 
 int populator(int usrCount,float splt)
 {
+	srand (time(NULL));
 	cout<<"Split Value = "<<splt*usrCount<<"\n";
 	for (int j=0,i = pos; i <= usrCount+pos; ++i,j++)
 	{
@@ -41,7 +44,9 @@ int data_disp()
 	{
 		if(data[i][0]<10)cout<<"0"<<data[i][0];
 		else cout<<data[i][0];
-		cout<<" "<<data[i][1]<<" "<<data[i][2]<<"\n";
+		cout<<"     "<<data[i][1]<<"     ";
+		if(data[i][2]==1)cout<<"YES\n";
+			else cout<<"NO\n";
 	}
 
 	cout<<"Preference Rate\nWebpage 1 = "<<choicefinder()*100<<"\nWebpage 2 = "<<100-choicefinder()*100<<"\n";
@@ -51,7 +56,12 @@ int main()
 {
 	int data[10000][3];
 
-	populator(20,0.5);
+	populator(500,0.5);
 	data_disp();
-	
+
+	while(1){
+	cin.get();
+	populator(500,choicefinder());
+	data_disp();
+	}
 }
